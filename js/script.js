711ad09248d4549950bf9fd5e7258e0bf8ee2c86
Rafael Cabral <rafaelcabralsilva1@gmail.com>
2024-09-09 22:12:22 -0300

    update

--- js/script.js
+++ js/script.js
@@ -1,2 +1,10 @@
 const statusPedido = ['coletado', 'preparacao', 'transito', 'Recebido na Região', 'A caminho', 'Entregue'];
+const empresa = ['Transporte PE'];
+const pedidos = {
+    '0001': { status: 'Coletado', progress: 10 },
+    '0002': { status: 'Preparação', progress: 30 },
+    '0003': { status: 'Trânsito', progress: 50 },
+    '0004': { status: 'Recebido na Região', progress: 70 },
+    '0005': { status: 'A caminho', progress: 90 }
+};
 
